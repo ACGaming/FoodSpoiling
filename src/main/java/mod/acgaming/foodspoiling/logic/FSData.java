6f2367ef650d150a5522125a094f0bcde9628d66
ACGaming <4818419+ACGaming@users.noreply.github.com>
2024-10-11 08:44:00 +0200

    Safer NBT tag sub-compound creation

--- src/main/java/mod/acgaming/foodspoiling/logic/FSData.java
+++ src/main/java/mod/acgaming/foodspoiling/logic/FSData.java
@@ -14,7 +14,7 @@ public class FSData
 
     public static void setCreationTime(ItemStack stack, long creationTime)
     {
-        NBTTagCompound tag = getNBTTag(stack);
+        NBTTagCompound tag = setNBTTagCompound(stack);
         tag.removeTag(TAG_REMAINING_LIFETIME);
         long roundedCreationTime = ((creationTime + FSConfig.GENERAL.checkIntervalInTicks / 2) / FSConfig.GENERAL.checkIntervalInTicks) * FSConfig.GENERAL.checkIntervalInTicks;
         tag.setLong(TAG_CREATION_TIME, roundedCreationTime);
@@ -22,55 +22,60 @@ public class FSData
 
     public static long getCreationTime(ItemStack stack)
     {
-        NBTTagCompound tag = getNBTTag(stack);
-        return tag.getLong(TAG_CREATION_TIME);
+        NBTTagCompound tag = getNBTTagCompound(stack);
+        return tag != null ? tag.getLong(TAG_CREATION_TIME) : 0;
     }
 
     public static boolean hasCreationTime(ItemStack stack)
     {
-        NBTTagCompound tag = getNBTTag(stack);
-        return tag.hasKey(TAG_CREATION_TIME);
+        NBTTagCompound tag = getNBTTagCompound(stack);
+        return tag != null && tag.hasKey(TAG_CREATION_TIME);
     }
 
     public static void setRemainingLifetime(ItemStack stack, int remainingLifetime)
     {
-        NBTTagCompound tag = getNBTTag(stack);
+        NBTTagCompound tag = setNBTTagCompound(stack);
         tag.removeTag(TAG_CREATION_TIME);
         tag.setInteger(TAG_REMAINING_LIFETIME, remainingLifetime);
     }
 
     public static int getRemainingLifetime(ItemStack stack)
     {
-        NBTTagCompound tag = getNBTTag(stack);
-        return tag.getInteger(TAG_REMAINING_LIFETIME);
+        NBTTagCompound tag = getNBTTagCompound(stack);
+        return tag != null ? tag.getInteger(TAG_REMAINING_LIFETIME) : 0;
     }
 
     public static boolean hasRemainingLifetime(ItemStack stack)
     {
-        NBTTagCompound tag = getNBTTag(stack);
-        return tag.hasKey(TAG_REMAINING_LIFETIME);
+        NBTTagCompound tag = getNBTTagCompound(stack);
+        return tag != null && tag.hasKey(TAG_REMAINING_LIFETIME);
     }
 
     public static void setID(ItemStack stack, int id)
     {
-        NBTTagCompound tag = getNBTTag(stack);
+        NBTTagCompound tag = setNBTTagCompound(stack);
         tag.setInteger(TAG_ID, id);
     }
 
     public static int getID(ItemStack stack)
     {
-        NBTTagCompound tag = getNBTTag(stack);
-        return tag.getInteger(TAG_ID);
+        NBTTagCompound tag = getNBTTagCompound(stack);
+        return tag != null ? tag.getInteger(TAG_ID) : 0;
     }
 
     public static boolean hasID(ItemStack stack)
     {
-        NBTTagCompound tag = getNBTTag(stack);
-        return tag.hasKey(TAG_ID);
+        NBTTagCompound tag = getNBTTagCompound(stack);
+        return tag != null && tag.hasKey(TAG_ID);
     }
 
-    private static NBTTagCompound getNBTTag(ItemStack stack)
+    private static NBTTagCompound setNBTTagCompound(ItemStack stack)
     {
         return stack.getOrCreateSubCompound(FoodSpoiling.MOD_ID);
+    }
+
+    private static NBTTagCompound getNBTTagCompound(ItemStack stack)
+    {
+        return stack.getSubCompound(FoodSpoiling.MOD_ID);
     }
 }
